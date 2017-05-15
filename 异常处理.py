5c35c24ae57e2484984aa88eeb5574e608095165
jeff <tae44@vip.qq.com>
2017-05-15 21:33:17 +0800

    assert

--- 异常处理.py
+++ 异常处理.py
@@ -10,3 +10,14 @@ except IndexError as e:
     print('索引错误')
 except Exception as e:
     print(e)
+else:
+    print('没毛病!')
+finally:
+    print('无论如何都执行')
+
+# assert 1 == 2
+
+# try:
+#     raise ValueError('hello')
+# except ValueError as e:
+#     print(e)
