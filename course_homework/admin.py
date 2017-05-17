cc06c684ec26548628a9ee579d89ecefd5a28192
syjiang <tae44@vip.qq.com>
2017-05-17 08:46:51 +0800

    homework

--- course_homework/admin.py
+++ course_homework/admin.py
@@ -1,9 +1,20 @@
+import time
+
 class Teacher:
     def __init__(self, name, sex, age):
         self.name = name
         self.sex = sex
         self.age = age
-        self.money = 0
+        self.__money = 0
+
+    def teach(self, student, course):
+        print('{}正在教{}{}...'.format(self.name, student.name, course.name))
+        time.sleep(2)
+        print('课时结束! {}赚了{}课时费~'.format(self.name, course.money))
+        self.__money += course.money
+
+    def howmany(self):
+        print('{}现有资产为{}元'.format(self.name, self.__money))
 
 class Course:
     def __init__(self, name, time, money, teacher):
@@ -11,4 +22,3 @@ class Course:
         self.time = time
         self.money = money
         self.teacher = teacher
-
--- course_homework/student.py
+++ course_homework/student.py
@@ -1,7 +1,16 @@
 class Student:
-    def __init__(self, name, sex, age, courseList, log):
+    def __init__(self, name, sex, age):
         self.name = name
         self.sex = sex
         self.age = age
-        self.courseList = courseList
-        self.log = log
+        self.courseList = [] # 学过的课程列表
+        self.log = []        # 学习记录
+
+    def select(self, course):
+        print('{}选择的课程是: {}'.format(self.name, course.name))
+        course.teacher.teach(self, course)
+        if course.name not in self.courseList:
+            self.courseList.append(course.name)
+
+    def learnWhat(self):
+        print('{}学过的课程为: {}'.format(self.name, self.courseList))
