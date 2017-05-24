12ae281135b18b565442f1b5e31ea2c8b95c599e
syjiang <tae44@vip.qq.com>
2017-05-24 16:33:29 +0800

    daemon

--- p_threading.py
+++ p_threading.py
@@ -1,13 +1,31 @@
 import threading
 import time
 
-def p(x):
-    time.sleep(1)
-    print(x)
-
+# def p(x):
+#     time.sleep(1)
+#     print(x)
+#
 # for i in range(10):
 #     p(i)
+#
+# for i in range(10):
+#     t = threading.Thread(target=p, args=(i,))
+#     t.start()
 
-for i in range(10):
-    t = threading.Thread(target=p, args=(i,))
-    t.start()
+def a():
+    print('start a')
+    time.sleep(5)
+    b()
+
+def b():
+    print('start b')
+
+t = threading.Thread(target=a)
+t.setDaemon(True) # 为True时主线程不等子线程退出再退出,默认FALSE
+t.start()
+t = threading.Thread(target=a)
+t.setDaemon(True)
+t.start()
+t = threading.Thread(target=a)
+t.setDaemon(True)
+t.start()
