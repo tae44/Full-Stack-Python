b103dabd2d437b8c7d8e1e56c68801b5852ea927
syjiang <tae44@vip.qq.com>
2017-05-24 16:45:00 +0800

    join

--- p_threading.py
+++ p_threading.py
@@ -14,18 +14,22 @@ import time
 
 def a():
     print('start a')
-    time.sleep(5)
+    time.sleep(3)
     b()
 
 def b():
     print('start b')
 
-t = threading.Thread(target=a)
-t.setDaemon(True) # 为True时主线程不等子线程退出再退出,默认FALSE
-t.start()
-t = threading.Thread(target=a)
-t.setDaemon(True)
-t.start()
-t = threading.Thread(target=a)
-t.setDaemon(True)
-t.start()
+t1 = threading.Thread(target=a)
+# t1.setDaemon(True) # 为True时主线程不等子线程退出再退出,默认FALSE
+t1.start()
+t1.join() # 等子线程结束再往下执行,不再并发执行
+
+t2 = threading.Thread(target=a)
+# t2.setDaemon(True)
+t2.start()
+t2.join(2) # 最多等2秒
+
+t3 = threading.Thread(target=a)
+# t3.setDaemon(True)
+t3.start()
