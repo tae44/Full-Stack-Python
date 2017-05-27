e588051f8e8deab61628a6cc3a6b2b09841c9aa7
syjiang <tae44@vip.qq.com>
2017-05-27 09:35:43 +0800

    apply_async

--- pool_of_workers.py
+++ pool_of_workers.py
@@ -4,48 +4,50 @@ import os
 
 # http://www.cnblogs.com/resn/p/5591419.html
 
-# def myFun(i):
-#     time.sleep(2)
-#     return i + 100
-#
-# def end_call(arg):
-#     print('end_call', arg)
-#
-# if __name__ == '__main__':
-#     p = Pool(5)
-#
-#     for i in range(10):
-#         p.apply_async(func=myFun, args=(i,), callback=end_call)
-#
-#     print('end')
-#     p.close()
-#     p.join()
+def myFun(i):
+    time.sleep(1)
+    # print(i + 100)
+    return i + 100
 
-def f(x):
-    return x*x
+def end_call(arg):
+    print('end_call', arg)
 
 if __name__ == '__main__':
-    with Pool(processes=4) as pool:
-        print(pool.map(f, range(10)))
+    p = Pool(5)
 
-        for i in pool.imap_unordered(f, range(10)):
-            print(i)
+    for i in range(10):
+        # p.apply(func=myFun, args=(i,)) # 一个一个执行
+        p.apply_async(func=myFun, args=(i,), callback=end_call) # 并发执行
 
-        res = pool.apply_async(f, (20,))
-        print(res.get(timeout=1))
+    print('end')
+    p.close()
+    p.join()
 
-        res = pool.apply_async(os.getpid)
-        print(res.get(timeout=1))
-
-        multiple_results = [pool.apply_async(os.getpid) for i in range(4)]
-        print([res.get(timeout=1) for res in multiple_results])
-
-        res = pool.apply_async(time.sleep, (5,))
-        try:
-            print(res.get(timeout=1))
-        except TimeoutError:
-            print('发现一个 multiprocessing.TimeoutError异常')
-
-        print('目前池中还有其他的工作')
-
-    print('Now the pool is closed and no longer available')
+# def f(x):
+#     return x*x
+#
+# if __name__ == '__main__':
+#     with Pool(processes=4) as pool:
+#         print(pool.map(f, range(10)))
+#
+#         for i in pool.imap_unordered(f, range(10)):
+#             print(i)
+#
+#         res = pool.apply_async(f, (20,))
+#         print(res.get(timeout=1))
+#
+#         res = pool.apply_async(os.getpid)
+#         print(res.get(timeout=1))
+#
+#         multiple_results = [pool.apply_async(os.getpid) for i in range(4)]
+#         print([res.get(timeout=1) for res in multiple_results])
+#
+#         res = pool.apply_async(time.sleep, (5,))
+#         try:
+#             print(res.get(timeout=1))
+#         except TimeoutError:
+#             print('发现一个 multiprocessing.TimeoutError异常')
+#
+#         print('目前池中还有其他的工作')
+#
+#     print('Now the pool is closed and no longer available')
